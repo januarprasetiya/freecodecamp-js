5232fc51366de569faae40b91f6f45581786ff28
januarprasetiya <prasetiyaadam@gmail.com>
2021-07-24 01:16:47 +0700

    Lanjut Javascript Basic

---
+++ basic.js
@@ -0,0 +1,123 @@
+// TODO Macam macam Komentar
+    //! Resolve
+    // This is an in-line comment.
+
+    /* This is a
+    multi-line comment */
+
+// ============================================================================================
+
+// TODO Variabel
+    //* Declare Javacript Variable
+    //! Resolve
+    var myName;
+
+    //* Contoh penulisan Variable yang lain
+    var januarPrastiya;
+    var genap;
+    var ganjil;
+    var nilaiRata;
+    // dan lain sebagainya
+
+// ============================================================================================
+
+// TODO Variabel
+    //* Memasukkan nilai variable
+    var a;
+
+    //! Resolve
+    a = 7;
+
+// ============================================================================================
+
+    //* Assigning the Value of One Variable to Another
+    // Setup
+    var a;
+    a = 7;
+    var b;
+
+    //! Resolve
+    b = a;
+
+// ============================================================================================
+
+    //* Initializing Variables with the Assignment Operator
+    // ? Define a variable a with var and initialize it to a value of 9
+
+    //! Resolve
+    var a = 9;
+
+// ============================================================================================
+
+    //*  Understanding Uninitialized Variables
+    //   Initialize the three variables a, b, and c with 5, 10, and "I am a" respectively so that they will not be undefined.
+    // ? a should be defined and evaluated to have the value of 6.
+    // ? b should be defined and evaluated to have the value of 15.
+    // ? c should not contain undefined and should have a value of the string I am a String!
+    // ? You should not change code below the specified comment.
+
+    //! Resolve
+    // * Only change code below this line
+    var a = 5;
+    var b = 10;
+    var c = "I am a";
+    
+    // Only change code above this line
+    a = a + 1;
+    b = b + 5;
+    c = c + " String!";
+
+// ============================================================================================
+
+    // * Understanding Case Sensitivity in Variables
+    // Example
+    var someVariable;
+    var anotherVariableName;
+    var thisVariableNameIsSoLong;
+
+
+    // ? Modify the existing declarations and assignments so their names use camelCase. 
+    // ? Do not create any new variables.
+
+    // ! Resolve
+    // Variable declarations
+    var studlyCapVar;
+    var properCamelCase;
+    var titleCaseOver;
+
+    // Variable assignments
+    studlyCapVar = 10;
+    properCamelCase = "A String";
+    titleCaseOver = 9000;
+
+// ============================================================================================
+
+    // * Add Two Numbers with JavaScript
+    // ? Change the 0 so that sum will equal 20.
+
+    // ! Resolve
+    var sum = 10 + 10;
+
+// ============================================================================================
+
+    // * Subtract One Number from Another with JavaScript
+    // ? Change the 0 so the difference is 12
+
+    // ! Resolve
+    var difference = 45 - 33;
+
+// ===========================================================================================
+
+    // * Multiply Two Numbers with JavaScript
+    // ? Change the 0 so that product will equal 80
+
+    // ! Resolve
+    var product = 8 * 10;
+
+// ===========================================================================================
+
+    // * Divide One Number by Another with JavaScript
+    // ? Change the 0 so that the quotient is equal to 2
+
+    // ! Resolve
+    var quotient = 66 / 33;
